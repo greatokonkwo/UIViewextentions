af46dbba8904994aecd2760d53d13823d65f60d3
Great <gboyokonkwo35@gmail.com>
2019-08-04 22:51:40 -0400

    Initial Commit

---
+++ Constraints/Constraints.h
@@ -0,0 +1,19 @@
+//
+//  Constraints.h
+//  Constraints
+//
+//  Created by Great Okonkwo on 8/4/19.
+//  Copyright © 2019 Great  Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Constraints.
+FOUNDATION_EXPORT double ConstraintsVersionNumber;
+
+//! Project version string for Constraints.
+FOUNDATION_EXPORT const unsigned char ConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Constraints/PublicHeader.h>
+
+
